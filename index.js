bc003713158c3896aaacb07bb2757ba8b2cd5a58
leah karienye <karienyeleah@gmail.com>
2022-06-16 23:58:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let names;
+  if (word = word.reverse){
+    names = true;
+  } else {
+    names = false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  console.log (wendy) expecting ((ydnew) false)
 */
 
 /*
   Add written explanation of your solution here
+  if the string passed is equa to the reverse of it then return true
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
