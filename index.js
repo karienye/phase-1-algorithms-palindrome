b62af1fa58ca95ba1637eb021af081b053398ff1
leah karienye <karienyeleah@gmail.com>
2022-06-17 00:31:39 +0300

    algorithms palindrome

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+function reverse(word){
+  const wordArray = word.split('');
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
-  let names;
-  if (word = word.reverse){
-    names = true;
+  const reversedWord = reverse (word)
+
+  if (word === reversedWord){
+    return true;
   } else {
-    names = false;
+    return false;
   }
 }
 
@@ -22,7 +30,7 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
